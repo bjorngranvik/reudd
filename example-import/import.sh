2b6f61af851d2712560b3f3b55dd6fef90bc3f58
bjorngranvik <bjorn.granvik@gmail.com>
2015-06-16 22:46:52 +0200

    Added review to test data.

--- example-import/import.sh
+++ example-import/import.sh
@@ -7,3 +7,9 @@
 groovy -cp target/production/reudd:/Users/bjorngranvik/.m2/repository/org/neo4j/neo4j-kernel/1.9.5/neo4j-kernel-1.9.5.jar:/Users/bjorngranvik/.m2/repository/javax/transaction/jta/1.1/jta-1.1.jar \
 src/groovy/org/reudd/Reudd import example-import/example-import-data_semicolon.csv
 
+# Schema
+groovy -cp /reudd/target/production/reudd:/Users/bjorngranvik/.m2/repository/org/neo4j/neo4j-kernel/1.9.5/neo4j-kernel-1.9.5.jar:/Users/bjorngranvik/.m2/repository/javax/transaction/jta/1.1/jta-1.1.jar \
+/reudd/src/groovy/org/reudd/Reudd schema example-import-data-model pdf
+
+groovy -cp /reudd/target/production/reudd:/Users/bjorngranvik/.m2/repository/org/neo4j/neo4j-kernel/1.9.5/neo4j-kernel-1.9.5.jar:/Users/bjorngranvik/.m2/repository/javax/transaction/jta/1.1/jta-1.1.jar \
+/reudd/src/groovy/org/reudd/Reudd schema example-import-data-model svg
